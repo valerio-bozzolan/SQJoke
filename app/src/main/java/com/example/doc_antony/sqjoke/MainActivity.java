1d0e2ce6c75bef5ac8f385fe49b5900cf9a991c1
Doc-Dev <antonio.cavuoto@gmail.com>
2016-05-02 14:16:08 +0200

    Beta2

--- app/src/main/java/com/example/doc_antony/sqjoke/MainActivity.java
+++ app/src/main/java/com/example/doc_antony/sqjoke/MainActivity.java
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package com.example.doc_antony.sqjoke;
 
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         button = (ImageButton) findViewById(R.id.ib1);
         button.setBackgroundResource(R.drawable.play);
         player = MediaPlayer.create(MainActivity.this, R.raw.music);
@@ -45,15 +47,17 @@ public class MainActivity extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
 
         if (hasFocus) {
-            getWindow().getDecorView()
-                    .setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    );
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                getWindow().getDecorView()
+                        .setSystemUiVisibility(
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        );
+            }
         }
     }
 
@@ -66,15 +70,18 @@ public class MainActivity extends AppCompatActivity {
             button.setBackgroundResource(R.drawable.pause);
 
         } else {
-            if (!paused) {
+            if (!paused && player.isPlaying()) {
                 paused = true;
                 player.pause();
                 button.setBackgroundResource(R.drawable.play);
+                clicked=false;
             } else {
-                player.start();
-                paused = false;
-                button.setBackgroundResource(R.drawable.play);
-
+                if(!player.isPlaying()) {
+                    player.start();
+                    paused = false;
+                    button.setBackgroundResource(R.drawable.play);
+                    clicked=true;
+                }
             }
         }
 
