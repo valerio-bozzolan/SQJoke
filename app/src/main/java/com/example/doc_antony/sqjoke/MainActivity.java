4824b8cd2cc5bfc9c5c8266de5c226447db458b9
doc-dev <antonio.cavuoto@gmail.com>
2016-05-03 19:08:35 +0200

    Release Beta2

--- app/src/main/java/com/example/doc_antony/sqjoke/MainActivity.java
+++ app/src/main/java/com/example/doc_antony/sqjoke/MainActivity.java
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package com.example.doc_antony.sqjoke;
 
+import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
